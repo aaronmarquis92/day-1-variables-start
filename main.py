b048a943679cdcb2426243b7c8ac00bac7e5423c
aaronmarquis92 <>
2021-06-08 10:32:52 +0000

    using variables to get the length of a name

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-input("What is your name?")
+#name = input("What is your name?")
+#print("Your name is: " + name)
+
+name = "Aaron"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("What is your name")
+length = len(name)
+print(name + ", your name is " + str(length) + " letters long")
